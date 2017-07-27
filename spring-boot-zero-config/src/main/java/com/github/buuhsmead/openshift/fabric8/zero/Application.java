0ffe59126db853e7120cf6f6a1381cfc886bf6ba
Huub Daems <buuhsmead@gmail.com>
2017-07-27 13:52:05 +0200

    Eerste opzet van spring boot zero.

---
+++ spring-boot-zero-config/src/main/java/com/github/buuhsmead/openshift/fabric8/zero/Application.java
@@ -0,0 +1,13 @@
+package com.github.buuhsmead.openshift.fabric8.zero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
