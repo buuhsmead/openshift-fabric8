1f225ef4da52b1d77cf08a84e3ed021077cd905e
Huub Daems <buuhsmead@gmail.com>
2017-08-23 16:45:42 +0200

    Add a Service and a Route definitions  through fabric8.

--- spring-boot-zero-config/src/main/java/com/github/buuhsmead/openshift/fabric8/zero/Application.java
+++ spring-boot-zero-config/src/main/java/com/github/buuhsmead/openshift/fabric8/zero/Application.java
@@ -2,6 +2,7 @@ package com.github.buuhsmead.openshift.fabric8.zero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ImportResource;
 public class Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 
